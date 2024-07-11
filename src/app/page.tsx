d9781c1209b386aca99c8bae2d03addfd7d51099
Devon Crebbin <devon@artvuu.group>
2024-07-11 22:29:12 +1000

    added placeholder image

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,9 +2,9 @@
 import { useRef } from "react";
 
 export default function Home() {
-  const generatedImage = useRef<HTMLImageElement>(null);
+  const generatedImage = useRef<HTMLImageElement>();
 
-  const inputRef = useRef<HTMLInputElement>(null);
+  const inputRef = useRef<HTMLInputElement>();
 
   function generateImage() {
     fetch("/api/font/generate", {
@@ -25,15 +25,16 @@ export default function Home() {
   }
 
   return (
-    <main className="flex min-h-screen flex-col items-center p-24">
+    <main className="flex min-h-screen flex-col items-center p-4">
+      <h1>Font Image Creator</h1>
       <div>
-        <input ref={inputRef} type="text" className="text-black" placeholder="请输入字符" />
+        <input ref={inputRef as any} type="text" className="text-black" placeholder="请输入字符" />
         <button className="bg-black text-white px-4 py-2 rounded-lg" onClick={generateImage}>
           Generate
         </button>
       </div>
       <div className="flex justify-center bg-white w-[400px] h-[300px] rounded-lg">
-        <img className="object-contain" width={200} height={200} ref={generatedImage} src="" alt="" />
+        <img className="object-contain" width={200} height={200} ref={generatedImage as any} src="/placeholder.png" alt="" />
       </div>
     </main>
   );
