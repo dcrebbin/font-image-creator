c8fcf1fc9981a6f11868da1d5ce8ab8938f49b70
Devon Crebbin <devon@artvuu.group>
2024-07-11 22:05:00 +1000

    added initial font generation api

---
+++ src/app/api/font/generate/route.ts
@@ -0,0 +1,32 @@
+import { createCanvas, registerFont } from "canvas";
+import path from "path";
+const fontPath = path.join(process.cwd(), "private", "fonts", `XiaolaiSC-Regular.ttf`);
+registerFont(fontPath, { family: "XiaolaiSC-Regular" as string });
+
+export async function POST(req: Request) {
+  const { characters } = await req.json();
+
+  try {
+    const characterLength = characters.length;
+    const characterWidth = characterLength * 40;
+    const canvas = createCanvas(characterWidth, 200);
+    const ctx = canvas.getContext("2d");
+    ctx.fillStyle = "white";
+    ctx.fillRect(0, 0, canvas.width, canvas.height);
+    ctx.font = `50px XiaolaiSC-Regular`;
+    ctx.fillStyle = "black";
+    ctx.fillText(characters, 20, 100);
+
+    const buffer = canvas.toBuffer("image/png");
+    const base64 = buffer.toString("base64");
+
+    return new Response(base64, {
+      headers: {
+        "Content-Type": "image/png",
+      },
+    });
+  } catch (error) {
+    console.error("Error generating image:", error);
+  }
+  return new Response("Error generating image", { status: 500 });
+}
